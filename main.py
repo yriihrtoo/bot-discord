b1b29f5b945ecd03512d1244224c4d0db4a02c8b
yriihrtoo <143616968+yriihrtoo@users.noreply.github.com>
2023-09-27 18:07:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,22 @@ async def on_message(message):
 async def on_message_delete(message):
         msg = f'{message.author} has deleted the message: {message.content}'
         await message.channel.send(msg)
+    
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
 
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
 client.run("MTE1MTUwMDQ4OTc4NDA0NTY2MQ.GIfVsr.SEjp0Fv8HvE3w_a1ANQx_8YCQGtdz8jL-ZLwqQ")
